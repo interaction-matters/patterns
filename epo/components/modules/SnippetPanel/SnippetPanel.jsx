158bc09e0b280c74b5d6205950ef6ade870fe218
ixMatters <konrad.spaeth@googlemail.com>
2015-11-27 08:47:16 +0100

    structural adjustments

--- epo/components/modules/SnippetPanel/SnippetPanel.jsx
+++ epo/components/modules/SnippetPanel/SnippetPanel.jsx
@@ -1,10 +1,9 @@
 /***********************
-Dossier Panel
-------------------------
-A Panel in Work Manager,
-showing the currently 
-selected dossier details
-************************/
+ Snippet Panel
+ ------------------------
+ A panel filled wwith snippets,
+ allowing to select the respective document
+ ************************/
 
 import React, { Component, PropTypes } from 'react';
 
@@ -15,18 +14,18 @@ import DossierPreview from 'components/modules/DossierPreview/DossierPreview';
 
 export default class DossierPanel extends Component {
 
-  render() {
+    render() {
 
-    let className = 'dossier-panel';
+        let className = 'dossier-panel';
 
-    return (
-      <div className={className}>
-        <AllocationModule />
-        <hr />
-        <DossierPreview />
-      </div>
-    );
-  }
+        return (
+            <div className={className}>
+                <AllocationModule />
+                <hr />
+                <DossierPreview />
+            </div>
+        );
+    }
 
 };
 
--- epo/pages/management/Management.jsx
+++ epo/pages/management/Management.jsx
@@ -15,20 +15,30 @@ export default class Management extends Component {
 
     return (
       <div className="management">
+
         <Panel panelName='management__main-panel'>
+
           {/* Absolute 'vanilla' implementation */}
           <DossierSnippet />
+
           {/* Selected state set to true, TA stamp visible, title present */}
           <DossierSnippet selected={true} taStamp={true} title="Titanium Alloy Composite for Reinforced Strategy"/>
+
           {/* With some stickers */}
           <DossierSnippet stickers={ [ {name:'ABEX'}, {name:'Divisional'}, {name:'SeCa'} ] } />
+
           {/* With number type, language set to German */}
           <DossierSnippet numberType="EP" language="DE"/>
+
           {/* With number type set to PCT  */}
           <DossierSnippet numberType="PCT" number="PCT 1234 5678A"/>
+
           {/* Finally, with some dates included  */}
           <DossierSnippet internalLimit='14.12.2015' committedLimit='10.03.2016' legalLimit='10.03.2018'/>
-    		</Panel>
+
+        </Panel>
+
+
         <Panel panelName='management__secondary-panel'>
     			<DossierPanel />
         </Panel>
